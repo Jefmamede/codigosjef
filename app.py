596b041bd88860b916168947c3ce6019a88553d3
Jefmamede <54160317+Jefmamede@users.noreply.github.com>
2021-06-17 15:54:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/') #Este comando cria uma página 
+def index(): # Mostrando um texto na página criada
 	return "Hello World"
